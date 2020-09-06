327fa2e41c5376052414c16b6ff4fec15f394b6d
Lynn Sakashita <dev.lynn.sakashita@gmail.com>
2020-09-06 20:22:49 +0900

    Introduce prisma context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
 let links = [{
   id: 'link-0',
@@ -11,24 +12,30 @@ let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: () => async (parent, args, context) =>
+    context.prisma.link.findMany()
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
-  }
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
+  },
 }
+
+const prisma = new PrismaClient()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {
+    prisma,
+  },
 })
 
 server.start(() => console.log(`Serevr is running on http://localhost:4000`))
